e0c2b6c02101b25582b46e1a74938216464d1fc3
Diver <teamextrem@mail.ru>
2023-12-20 23:26:40 +0300

    ДОБАВЛЕНО модальное окно к QR-коду, ИЗМЕНЕН цвет хедера.

---
+++ JS/qr_Window.js
@@ -0,0 +1,30 @@
+const buttonModal = document.getElementById('phoneBlock'); 
+const modalWindow = document.getElementById('qrWindow');
+const modalContent = document.getElementById('qrContent');
+const modalClose = document.getElementById('modalClose');
+
+// console.log(buttonModal);
+// console.log(modalWindow);
+// console.log(modalContent);
+// console.log(modalClose);
+
+
+buttonModal.addEventListener('click', () => {
+    modalWindow.style.opacity = '1';
+    modalWindow.style.visibility = 'visible';
+    modalContent.style.transform = 'translateY(0)';
+});
+
+modalClose.addEventListener('click', () =>{
+    modalWindow.style.opacity = '0';
+    modalWindow.style.visibility = 'hidden';
+    modalContent.style.transform = 'translateY(-50%)';
+});
+
+window.addEventListener('click', (event) =>{
+    if(event.target == modalWindow){
+        modalWindow.style.opacity = '0';
+        modalWindow.style.visibility = 'hidden';
+        modalContent.style.transform = 'translateY(-50%)';
+    }
+});
